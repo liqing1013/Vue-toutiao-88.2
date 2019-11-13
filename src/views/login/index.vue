fe36ab7448282bc820a56b3323e008f0e5aa5c66
liqing1013 <936012165@qq.com>
2019-11-13 22:42:18 +0800

    黑马头条PC-登录模块-数据绑定及校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -7,23 +7,25 @@
         <img src="../../assets/img/logo_index.png" alt />
       </div>
       <!-- 表单登录信息填写 -->
-      <el-form style="margin-top:10px">
+      <!-- model属性绑定表单数据 -->
+      <el-form :model="formData" style="margin-top:10px" :rules="formRules" ref='dataObj'>
+        <!-- ---------------------------------form-item的prop设置为校验的字段名 -->
         <!-- 手机号填写 -->
-        <el-form-item>
-          <el-input placeholder="请输入手机号"></el-input>
+        <el-form-item prop="mobile">
+          <el-input placeholder="请输入手机号" v-model="formData.mobile"></el-input>
         </el-form-item>
         <!-- 验证码填写 -->
-        <el-form-item>
-          <el-input style="width:180px" placeholder="验证码"></el-input>
+        <el-form-item prop="msg">
+          <el-input style="width:180px" placeholder="验证码" v-model="formData.msg"></el-input>
           <el-button style="float:right">发送验证码</el-button>
         </el-form-item>
         <!-- 同意协议条框 -->
-        <el-form-item>
-          <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+        <el-form-item prop="checked">
+          <el-checkbox v-model="formData.checked">我已阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
         <!-- 登录按钮 -->
         <el-form-item>
-          <el-button style="width:100%" type="primary">登录</el-button>
+          <el-button style="width:100%" type="primary" @click='login'>登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -31,7 +33,68 @@
 </template>
 
 <script>
-export default {}
+export default {
+  // -----------------------------------------------设置需要校验的整个表单信息
+  data () {
+    return {
+      formData: {
+        mobile: '',
+        msg: '',
+        checked: false
+      },
+      // -----------------------------------------------约定验证规则
+      formRules: {
+        // 手机号验证规则-pattern正则
+        mobile: [
+          {
+            required: true,
+            message: '请输入手机号'
+          },
+          {
+            pattern: /^1[3456789]\d{9}$/,
+            message: '请输入正确的手机号'
+          }
+        ],
+        // 验证码验证规则
+        msg: [
+          {
+            required: true,
+            message: '验证码'
+          },
+          {
+            pattern: /^\d{6}$/,
+            message: '请6位验证码'
+          }
+        ],
+        // 验证同意框是否勾选:rule-当前规则/value-当前值/callback-回调函数
+        checked: [
+          {
+            validator: function (rule, value, callback) {
+              // 如果当前值返回true,执行函数
+              if (value) {
+                callback()
+              } else {
+                callback(new Error('请您勾选用户协议和隐私条款'))
+              }
+            }
+          }
+        ]
+      }
+    }
+  },
+  // 给登录注册事件，对整个表单进行校验的方法，参数为一个回调函数。该回调函数会在校验结束后被调用，并传入两个参数：
+  // 是否校验成功和未通过校验的字段。若不传入回调函数，则会返回一个 promise
+  methods: {
+    login () {
+      this.$refs.dataObj.validate((ifOK) => {
+        // 如果返回true,则进入主页
+        if (ifOK) {
+          this.$router.push('/home')
+        }
+      })
+    }
+  }
+}
 </script>
 
 <style lang='less' scoped>

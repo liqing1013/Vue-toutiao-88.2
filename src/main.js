07a8a5841dcbb3663a8af0044604ac96743c7752
liqing1013 <936012165@qq.com>
2019-11-13 17:29:35 +0800

    黑马头条PC-初始化项目-页面分析-新建登录和主页

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 下载安装element-ui
+import ElementUI from 'element-ui'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)// 将elementui定义到全局，任何组件都可使用
 
 new Vue({
   router,
